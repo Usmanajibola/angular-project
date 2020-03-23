a9e468dbcdc0db085aba9094f933df5a8326088d
usmanajibola <usmanajibolaabass@gmail.com>
2020-03-23 19:51:32 +0100

    updated my code

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { WebsocketService } from '../websocket.service';
 import { Scrumuser } from '../scrumuser';
 
 @Component({
@@ -7,78 +9,87 @@ import { Scrumuser } from '../scrumuser';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-
-  WS_URL: string = 'wss://667kkhuds9.execute-api.us-east-2.amazonaws.com/test'
-  websocketConnection:WebSocket
-  logincred:any;
-  chat_text:String
-
+  WS_URL: String = 'wss://667kkhuds9.execute-api.us-east-2.amazonaws.com/test'
   messages = [];
+  chat_text: String
+  logincred: any;
+  messageFromServer: String;
+  wsSubscription: Subscription;
+  status: any;
+  socket: WebSocket;
 
-  constructor(
+  constructor(private wsService: WebsocketService) {
+    // this.wsSubscription = this.wsService.createObservableSocket("wss://0l90clyplf.execute-api.us-east-2.amazonaws.com/test").subscribe(
+    //   data =>{
+    //     this.messageFromServer = data
+    //   },
+    //   err =>{
+    //     console.log('err', err);
+    //   }
+    // )
+    this.socket = new WebSocket('wss://667kkhuds9.execute-api.us-east-2.amazonaws.com/test');
+    console.log('connect', this.socket)
 
-
-  ) {
-    this.websocketConnection = new WebSocket(this.WS_URL)
   }
 
   ngOnInit(): void {
-    this.websocketConnection.onopen=(event)=>{
-       const context = {action:'getRecentMessages'}
-       this.websocketConnection.send(JSON.stringify(context))
+    this.socket.onopen = (event) => {
+      const context = { action: "getRecentMessages" }
+      this.socket.send(JSON.stringify(context))
 
+      this.socket.onmessage = (event) =>{
+        let data = JSON.parse(event.data)
+        console.log("message things" + data['messages'])
+        if (data['messages'] !== undefined){
+          data['messages'].forEach((message) => {
+            console.log("new =" + JSON.stringify(message))
+            this.messages.push(message)
 
+          })
+          console.log("Array "+ this.messages)
+          this.messages.forEach((message) =>{
+            console.log("allmessages " + message.username )
+          })
+        }
+      }
     }
-    this.websocketConnection.onmessage=(event)=>{
-       let data=JSON.parse(event.data)
-
-       if  (data['messages']!==undefined) {
-         data['messages'].forEach((message)=>{
-           this.messages.push(message);
-         })
-       }
-       console.log(this.messages);
-
-    }
-
-
-
   }
 
- getUsername(): string{
-    this.logincred = JSON.parse(localStorage.getItem('Authobj'));
-    return this.logincred.name;
-  };
+  scrumLoginUserModel = new Scrumuser('', '', '', '', '');
 
-  userLoginData = new Scrumuser('', '','','', '')
-
-getCurrentTime() {
-  return new Date().toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
-}
-
-
-  sendMessage(chat_text) {
+  getUsername(): string {
+   this.logincred = JSON.parse(localStorage.getItem('Authobj'));
+   console.log("The User ="+this.logincred.name)
+   return this.logincred.name
+ }
+ getCurrentTime(){
+   return new Date().toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
+ }
 
 
+ sendMessage() {
+   const chat_text = this.scrumLoginUserModel.projectname;
 
-    if (chat_text) {
-      const context = {"action":"sendMessage", "content":chat_text, "username":this.getUsername(), "timestamp":this.getCurrentTime()}
-      let strContext = JSON.stringify(context);
-      this.websocketConnection.send(strContext);
-      this.chat_text = '';
-      this.userLoginData.projectname = '';
-
-      window.setInterval(function () {
+   if(chat_text){
+     const context = {
+     "action": "sendMessage",
+     "username": this.getUsername(),
+     "content": chat_text,
+     "timestamp": this.getCurrentTime() }
+     const cc = JSON.stringify(context);
+     this.socket.send(cc)
+     this.chat_text =''
+     this.scrumLoginUserModel.projectname = '';
+   window.setInterval(function () {
      const elem = document.getElementById('data');
      elem.scrollTop = elem.scrollHeight;
    }, 5000);
+   }
+
+ }
 
 
-
-    }
-  }
-
-  ngOnDestroy() {
-    this.websocketConnection.close();
-  }
+ ngOnDestroy(){
+   this.socket.close()
+ }
 }
